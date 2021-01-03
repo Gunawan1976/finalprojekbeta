056f48c586badf277311ccc74aa3989c46f7d635
Gunawan1976 <arig85245@gmail.com>
2021-01-03 19:30:30 +0700

    mumet

--- app/src/main/java/com/mobile/finalprojekbetaa/ui/dashboard/HitoryFragment.java
+++ app/src/main/java/com/mobile/finalprojekbetaa/ui/dashboard/HitoryFragment.java
@@ -4,21 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.mobile.finalprojekbetaa.R;
 
-public class DashboardFragment extends Fragment {
+public class DashboardHitory extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View root = inflater.inflate(R.layout.fragment_history, container, false);
         return root;
     }
 }
